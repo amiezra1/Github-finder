7bb81c838845f7a37b56b639fbf227e24140eab6
amiezra1 <myzr2425@gmail.com>
2024-04-05 14:11:45 +0300

    search and clear option

--- src/App.js
+++ src/App.js
@@ -10,19 +10,28 @@ class App extends Component {
     users: [],
     loading: false,
   };
-  async componentDidMount() {
+
+  //Search Github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 
 export class Search extends Component {
   state = {
-    test: '',
+    text: '',
+  };
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearhUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onSubmit = (e) => {
     e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -19,7 +27,7 @@ export class Search extends Component {
             type='text'
             name='text'
             placeholder='Search Users...'
-            value={this.state.test}
+            value={this.state.text}
             onChange={this.onChange}
           />
           <input
@@ -28,6 +36,14 @@ export class Search extends Component {
             className='btn btn-block btn-dark'
           />
         </form>
+        {this.props.showClear && (
+          <button
+            className='btn btn-light btn-block'
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
