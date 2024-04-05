dbd740241fbd28d48a1ac0d91ccf75aecc646c4f
amiezra1 <myzr2425@gmail.com>
2024-04-05 15:08:36 +0300

    destruction for showClear and clearUsers

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -20,6 +20,7 @@ export class Search extends Component {
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} action='form'>
@@ -36,11 +37,8 @@ export class Search extends Component {
             className='btn btn-block btn-dark'
           />
         </form>
-        {this.props.showClear && (
-          <button
-            className='btn btn-light btn-block'
-            onClick={this.props.clearUsers}
-          >
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
             Clear
           </button>
         )}
--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const Useritem = ({ user: { login, avatar_url, htmk_url } }) => {
+const Useritem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <div className='card text-center'>
       <img
@@ -12,7 +12,7 @@ const Useritem = ({ user: { login, avatar_url, htmk_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href={htmk_url} className='btn btn-dark btn-sm my-1'>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
           More
         </a>
       </div>
